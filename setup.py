57870e170c346f6b2e8420b562186b7043225d87
Vivien Barousse <vivien@hubblehq.com>
2020-03-06 14:14:39 +0000

    Prepare for release 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hubble_shuttle",
-    version="0.1",
+    version="0.1.1",
     author="HubbleHQ",
     author_email="dev@hubblehq.com",
     description="Hubble's Shuttle",
