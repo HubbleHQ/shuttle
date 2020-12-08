4b8d80fc4f1c075c8e7c213e8be24567c7a819bb
Isabela Hodorog <isabela.hodorog@gmail.com>
2020-12-08 10:20:35 +0000

    Increase version number to 0.2.0 in setup.py
    In a previous PR I added a 409 ConflictError in the exceptions file.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hubble_shuttle",
-    version="0.1.2",
+    version="0.2.0",
     author="HubbleHQ",
     author_email="dev@hubblehq.com",
     description="Hubble's Shuttle",
