9ebbf354425b19861775d2142fae0947a0822b8c
Isabela Hodorog <isabela.hodorog@gmail.com>
2023-08-23 15:52:40 +0100

    feat: Bump the package to a higher version
    Dropping support for python 3.7 is a big change, so this is needed.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hubble_shuttle",
-    version="0.3.0",
+    version="0.4.0",
     author="HubbleHQ",
     author_email="dev@hubblehq.com",
     description="Hubble's Shuttle",
